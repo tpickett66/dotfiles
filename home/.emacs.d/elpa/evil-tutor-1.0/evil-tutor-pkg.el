4a07c08eda6f8300ec8ea9a709428282d052e7e7
Tyler Pickett <t.pickett66@gmail.com>
2016-04-14 12:34:48 -0600

    start playing w/ emacs again...

---
+++ home/.emacs.d/elpa/evil-tutor-1.0/evil-tutor-pkg.el
@@ -0,0 +1,7 @@
+(define-package "evil-tutor" "1.0" "Vimtutor adapted to Evil and wrapped in a major-mode"
+  '((evil "1.0.9"))
+  :url "https://github.com/syl20bnr/evil-tutor" :keywords
+  '("convenience" "editing" "evil"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
