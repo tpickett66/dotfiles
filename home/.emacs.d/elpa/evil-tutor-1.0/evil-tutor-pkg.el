14b696269f97dc9204764168adb4e0c5df077e39
Tyler Pickett <tpickett@instructure.com>
2020-06-07 09:10:52 -0500

    I'm never going to use emacs, clean it up

--- home/.emacs.d/elpa/evil-tutor-1.0/evil-tutor-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "evil-tutor" "1.0" "Vimtutor adapted to Evil and wrapped in a major-mode"
-  '((evil "1.0.9"))
-  :url "https://github.com/syl20bnr/evil-tutor" :keywords
-  '("convenience" "editing" "evil"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
